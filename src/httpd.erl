5e92d61028c2c43deb132996fd0c7b63114e6178
harmon25 <dougwright1@gmail.com>
2026-01-11 15:00:23 -0500

    Add support for socket options in HTTP server start functions
    Signed-off-by: harmon25 <dougwright1@gmail.com>

--- src/httpd.erl
+++ src/httpd.erl
@@ -17,7 +17,7 @@
 
 -module(httpd).
 
--export([start/2, start/3, start_link/2, start_link/3, stop/1]).
+-export([start/2, start/3, start/4, start_link/2, start_link/3, start_link/4, stop/1]).
 
 -behaviour(gen_tcp_server).
 -export([init/1, handle_receive/3, handle_tcp_closed/2]).
@@ -76,19 +76,27 @@
 
 -spec start(Port :: portnum(), Config :: config()) -> {ok, HTTPD :: pid()} | {error, Reason :: term()}.
 start(Port, Config) ->
-    start(any, Port, Config).
+    start(any, Port, #{}, Config).
 
 -spec start(Address :: address(), Port :: portnum(), Config :: config()) -> {ok, HTTPD :: pid()} | {error, Reason :: term()}.
 start(Address, Port, Config) ->
-    gen_tcp_server:start(#{addr => Address, port => Port}, ?MODULE, Config).
+    start(Address, Port, #{}, Config).
+
+-spec start(Address :: address(), Port :: portnum(), SocketOptions :: map(), Config :: config()) -> {ok, HTTPD :: pid()} | {error, Reason :: term()}.
+start(Address, Port, SocketOptions, Config) ->
+    gen_tcp_server:start(#{addr => Address, port => Port}, SocketOptions, ?MODULE, Config).
 
 -spec start_link(Port :: portnum(), Config :: config()) -> {ok, HTTPD :: pid()} | {error, Reason :: term()}.
 start_link(Port, Config) ->
-    start_link(any, Port, Config).
+    start_link(any, Port, #{}, Config).
 
 -spec start_link(Address :: address(), Port :: portnum(), Config :: config()) -> {ok, HTTPD :: pid()} | {error, Reason :: term()}.
 start_link(Address, Port, Config) ->
-    gen_tcp_server:start_link(#{addr => Address, port => Port}, ?MODULE, Config).
+    start_link(Address, Port, #{}, Config).
+
+-spec start_link(Address :: address(), Port :: portnum(), SocketOptions :: map(), Config :: config()) -> {ok, HTTPD :: pid()} | {error, Reason :: term()}.
+start_link(Address, Port, SocketOptions, Config) ->
+    gen_tcp_server:start_link(#{addr => Address, port => Port}, SocketOptions, ?MODULE, Config).
 
 stop(Httpd) ->
     gen_tcp_server:stop(Httpd).
@@ -604,6 +612,7 @@ maybe_binary_to_string(Other) ->
 %% @private
 to_headers_list(Headers) ->
     [io_lib:format("~s: ~s\r\n", [maybe_binary_to_string(Key), maybe_binary_to_string(Value)]) || {Key, Value} <- maps:to_list(Headers)].
+
 
 %% @private
 get_version_str(Version) when is_binary(Version) ->
